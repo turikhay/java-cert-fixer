3238f5f099a7993abf19e9d8cf551e19f7021b21
turikhay <git@turikhay.com>
2022-06-02 19:24:03 +0300

    ConsumerBasedLogger

--- src/main/java/com/turikhay/caf/util/Logger.java
+++ src/main/java/com/turikhay/caf/util/Logger.java
@@ -1,11 +1,62 @@
 package com.turikhay.caf.util;
 
 import java.io.PrintStream;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public interface Logger {
     void logMessage(String message);
 
     void logError(String message, Throwable error);
+
+    static ConsumerBasedLogger.Builder builder() {
+        return new ConsumerBasedLogger.Builder();
+    }
+
+    class ConsumerBasedLogger implements Logger {
+        private final Consumer<String> logMessageConsumer;
+        private final BiConsumer<String, Throwable> logErrorConsumer;
+
+        private ConsumerBasedLogger(Consumer<String> logMessageConsumer, BiConsumer<String, Throwable> logErrorConsumer) {
+            if (logMessageConsumer == null) {
+                throw new NullPointerException("logMessageConsumer");
+            }
+            if (logErrorConsumer == null) {
+                throw new NullPointerException("logErrorConsumer");
+            }
+            this.logMessageConsumer = logMessageConsumer;
+            this.logErrorConsumer = logErrorConsumer;
+        }
+
+        @Override
+        public void logMessage(String message) {
+            logMessageConsumer.accept(message);
+        }
+
+        @Override
+        public void logError(String message, Throwable error) {
+            logErrorConsumer.accept(message, error);
+        }
+
+        static class Builder {
+            private Consumer<String> logMessageConsumer;
+            private BiConsumer<String, Throwable> logErrorConsumer;
+
+            public Builder logMessage(Consumer<String> logMessageConsumer) {
+                this.logMessageConsumer = logMessageConsumer;
+                return this;
+            }
+
+            public Builder logError(BiConsumer<String, Throwable> logErrorConsumer) {
+                this.logErrorConsumer = logErrorConsumer;
+                return this;
+            }
+
+            public ConsumerBasedLogger build() {
+                return new ConsumerBasedLogger(logMessageConsumer, logErrorConsumer);
+            }
+        }
+    }
 
     class PrintLogger implements Logger {
         private final PrintStream messageStream, errorStream;
